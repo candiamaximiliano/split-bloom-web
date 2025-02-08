5aac761711e086a097beacdc46c894f21949f8fe
Maximiliano Candia <candiamaximiliano.dev@gmail.com>
2025-02-07 21:26:13 -0300

    fix(layout.tsx): wrap children in a main element with responsive padding for better layout consistency

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -165,7 +165,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <Navigation />
-          {children}
+          <main className="px-4 sm:px-6 md:px-8 lg:px-12">{children}</main>
           <Footer />
         </ThemeProvider>
       </body>
